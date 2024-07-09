69ff1739904f6253be39d3730a1b10958e247970
Fash Muyhee <fashmuyidavid@gmail.com>
2024-07-09 20:40:01 +0100

    change model

--- src/hook/chat/useAskGemini.tsx
+++ src/hook/chat/useAskGemini.tsx
@@ -4,7 +4,7 @@ import { Content, GoogleGenerativeAI } from '@google/generative-ai';
 const apiKey = process.env.EXPO_PUBLIC_GEMINI_KEY;
 const genAI = new GoogleGenerativeAI(apiKey ?? '');
 const model = genAI.getGenerativeModel({
-  model: 'gemini-1.5-flash',
+  model: 'gemini-1.5-pro',
 });
 
 const generationConfig = {
