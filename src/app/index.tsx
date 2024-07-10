300ddcfe93a5ddffdfdfa3a1e8aaca59ad6edac0
Fash Muyhee <fashmuyidavid@gmail.com>
2024-07-10 12:11:05 +0100

    add redirect

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,18 @@
 import { View, StyleSheet } from 'react-native';
 import React from 'react';
 import { AnimatedIntro, IntroBottomSheet } from '@components/intro';
+import { useAuth } from '@clerk/clerk-expo';
+import { Redirect } from 'expo-router';
 
 type Props = {};
 
 const Intro = (props: Props) => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/(chat)/" />;
+  }
+
   return (
     <View style={styles.container}>
       <AnimatedIntro />
